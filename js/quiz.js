e0cfae960e6cfbe0b87b428bececc6cc5684ef54
MrJop <jop@interactionfigure.nl>
2024-12-06 13:39:11 +0100

    working

--- js/quiz.js
+++ js/quiz.js
@@ -5,14 +5,17 @@ $(document).ready(function(e) {
 });
 
 var SiteManager = {
+
+    //
     myQuizContainer: $('.js-quiz'),
     quizData:null,
     myQuestions:null,
     myNextButtons: null,
+    myRadioButtons: null,
     myStartButton: $('.js-start-button'),
     myStartScreen: $('.js-start-screen'),
     nCurrentQuestion:-1,
-    oScore:{},
+    aAnswers: [],
 
     init: function() {
         $.getJSON( "data/data.json", this.onDataLoaded.bind(this) );
@@ -37,8 +40,9 @@ var SiteManager = {
                     for(var j=0;j<q.answers.length;j++){
                         var mcID = `mc_${i}_${j}`;
                         input += `
-                            <input type="radio" id="${mcID}" name="${mcName}">
-                            <label for="${mcID}">${q.answers[j].text}</label><br>
+                            <div class="js-radio-button">
+                            <input type="radio" id="${mcID}" name="${mcName}" value="${j}">
+                            <label for="${mcID}">${q.answers[j].text}</label></div><br>
                         `;
                     }
                     break;
@@ -63,18 +67,12 @@ var SiteManager = {
     },
 
     onStartClicked: function () {
-        this.oScore = {
-            "spaarder": 0,
-            "budgetplanner": 0,
-            "geldsmijter": 0,
-            "impulsievekoper": 0,
-            "investeerder": 0,
-            "nonchalant": 0
-        };
         //
         this.myStartButton.off('click');
         this.myStartScreen.addClass('--hide');
         this.myStartScreen.removeClass('--show');
+        //
+        this.aAnswers = [];
         //
         this.gotoNextQuestion();
     },
@@ -87,22 +85,86 @@ var SiteManager = {
         //
         this.nCurrentQuestion++;
         //
-        $(this.myQuestions[this.nCurrentQuestion]).addClass('--show');
+        if(this.nCurrentQuestion < this.quizData.questions.length) {
+            $(this.myQuestions[this.nCurrentQuestion]).addClass('--show');
+            //
+            switch(this.quizData.questions[this.nCurrentQuestion].type){
+                case 'slider':
+                    break;
+                case 'multiple-choice':
+                    this.myRadioButtons = $(this.myQuestions[this.nCurrentQuestion]).find('.js-radio-button');
+                    this.myRadioButtons.on('click', this.onRadioButtonClicked.bind(this));
+                    break;
+            }
+        }else{
+            this.gotoEndScreen();
+        }
+    },
+
+    gotoEndScreen: function () {
+        var results = {};
+        for(var i=0;i<this.quizData.personas.length;i++){
+            results[this.quizData.personas[i]] = 0;
+        }
+        //
+
+        for(var i=0;i<this.quizData.questions.length;i++){
+            var q = this.quizData.questions[i];
+            var score = q.answers[this.aAnswers[i]];
+            //
+            for(var j=0;j<this.quizData.personas.length;j++){
+                var personaID = this.quizData.personas[j];
+                results[personaID]+=score[personaID];
+            }
+        }
+
+        let sortable = [];
+        for (var persona in results) {
+            sortable.push([persona, results[persona]]);
+        }
+
+        sortable.sort(function(a, b) {
+            return b[1] - a[1];
+        });
+
+        $('.js-end-screen').append(`<ol>`);
+        for(var i=0;i<sortable.length;i++){
+            $('.js-end-screen').append(`<li><strong>${sortable[i][0]}</strong>: ${sortable[i][1]}</li>`);
+        }
+        $('.js-end-screen').append(`</ol>`);
+
+        $('.js-end-screen').addClass('--show');
     },
 
     storeAnswer: function(){
         var q = this.quizData.questions[this.nCurrentQuestion];
+        var answer;
         switch(q.type) {
             case "slider":
                 var v = $($(this.myQuestions[this.nCurrentQuestion]).find('input')).val();
                 var divider =  100 / (q.answers.length - 1);
-                console.log(v/divider);
+                answer = v/divider;
+                break;
 
+            case "multiple-choice":
+                var inputs = $($(this.myQuestions[this.nCurrentQuestion]).find('input'));
+                answer = inputs.filter(":checked").val();
                 break;
         }
+        this.aAnswers.push(answer);
     },
 
     onNextClicked: function () {
+        this.storeAnswer();
+        this.gotoNextQuestion();
+    },
+
+    onRadioButtonClicked: function () {
+        this.myRadioButtons.off('click');
+        window.setTimeout(this.onRadioClickDelayDone.bind(this), 40);
+    },
+
+    onRadioClickDelayDone: function () {
         this.storeAnswer();
         this.gotoNextQuestion();
     }
